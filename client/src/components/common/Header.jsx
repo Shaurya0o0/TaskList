531236e1572e0d1b08401c672fdc079aa96cd2c0
Shaurya0o0 <shauryaprashad@gmail.com>
2026-02-23 22:35:00 +0530

    changed Logo Name

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Brand */}
         <h1 className="text-xl font-semibold text-blue-600">
-          NoteME
+          TaskList
         </h1>
 
         {user && (
